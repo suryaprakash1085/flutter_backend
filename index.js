a38f3a4637145f955a6e31ead6e8bbedcc9803bd
Suriya <prakashsuriya3257@gmail.com>
2025-04-29 10:52:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
